5d68373a028ea2868fb506ebfcdca1254d700727
Hallvar Haugdal <hallvar@Hallvar-sin-MacBook-Pro.local>
2023-03-08 14:47:54 +0100

    Added example

---
+++ examples/gov_sine_excitation/short_circuit.py
@@ -0,0 +1,76 @@
+import sys
+from collections import defaultdict
+import matplotlib.pyplot as plt
+import time
+import dynpssimpy.dynamic as dps
+import dynpssimpy.solvers as dps_sol
+import importlib
+importlib.reload(dps)
+
+if __name__ == '__main__':
+
+    # Load model
+    import dynpssimpy.ps_models.ieee39 as model_data
+    importlib.reload(model_data)
+    model = model_data.load()
+
+    # Import user defined models
+    import user_lib
+    print(user_lib)
+    print(user_lib.gov)
+    print(user_lib.gov.TGOV1)
+
+    # Power system model
+    ps = dps.PowerSystemModel(model=model, user_mdl_lib=user_lib)
+    ps.init_dyn_sim()
+    print(max(abs(ps.state_derivatives(0, ps.x_0, ps.v_0))))
+
+    t_end = 20
+    x_0 = ps.x_0.copy()
+
+    # Solver
+    sol = dps_sol.ModifiedEulerDAE(ps.state_derivatives, ps.solve_algebraic, 0, x_0, t_end, max_step=5e-3)
+
+    # Initialize simulation
+    t = 0
+    res = defaultdict(list)
+    t_0 = time.time()
+
+    sc_bus_idx = ps.gen['GEN'].bus_idx_red['terminal'][0]
+
+    import numpy as np
+    omega = 2*np.pi*0.2
+    amplitude = 0.001
+
+    # Run simulation
+    while t < t_end:
+        sys.stdout.write("\r%d%%" % (t/(t_end)*100))
+
+        # Sinusoidal excitation:
+        ps.gov['TGOV1'].set_input(input_name='aux_input', value=amplitude*np.sin(omega*t), idx=0)
+        
+        # Short circuit
+        # if t >= 1 and t <= 1.05:
+        #     ps.y_bus_red_mod[(sc_bus_idx,) * 2] = 1e6
+        # else:
+        #     ps.y_bus_red_mod[(sc_bus_idx,) * 2] = 0
+
+        # Simulate next step
+        result = sol.step()
+        x = sol.y
+        v = sol.v
+        t = sol.t
+
+        dx = ps.ode_fun(0, ps.x_0)
+
+        # Store result
+        res['t'].append(t)
+        res['gen_speed'].append(ps.gen['GEN'].speed(x, v).copy())
+
+    print('Simulation completed in {:.2f} seconds.'.format(time.time() - t_0))
+
+    plt.figure()
+    plt.plot(res['t'], res['gen_speed'])
+    plt.xlabel('Time [s]')
+    plt.ylabel('Gen. speed')
+    plt.show()
