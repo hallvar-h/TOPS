d3c2d5a51d2798edc0002f10a217a6b4eebf0591
Nora Lillelien <noralillelien@hotmail.com>
2024-11-07 08:34:28 +0100

    new SI loop with blocks

--- src/tops/dyn_models/vsc.py
+++ src/tops/dyn_models/vsc.py
@@ -1,5 +1,5 @@
 from tops.dyn_models.blocks import *
-from .pll import PLL1
+from .pll import PLL1, PLL1_nora
 
 class VSC(DAEModel):
     def __init__(self, *args, **kwargs):
@@ -16,9 +16,9 @@ class VSC(DAEModel):
         self.pll = PLL1(T_filter=self.par['T_pll'], bus=p['bus'])
 
         self.pi_p = PIRegulator(K_p=p['P_K_p'], K_i=p['P_K_i'])
-        self.pi_p.input = lambda x, v: self.P_setp(x, v) - self.P(x, v)
+        self.pi_p.input = lambda x, v: self.P_setp(x, v) - self.P(x, v) 
 
-        self.pi_q = PIRegulator(K_p=p['Q_K_p'], K_i=p['Q_K_i'])
+        self.pi_q = PIRegulator(K_p=p['Q_K_p'], K_i=p['Q_K_i']) 
         self.pi_q.input = lambda x, v: self.Q_setp(x, v) - self.Q(x, v)
 
         self.lag_p = TimeConstant(T=p['T_i'])
@@ -435,3 +435,75 @@ class VSC_PQ(DAEModel):
 
     def q_e(self, x, v):
         return self.s_e(x,v).imag
+
+
+class VSC_nora(DAEModel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.bus_idx = np.array(np.zeros(self.n_units), dtype=[(key, int) for key in self.bus_ref_spec().keys()])
+        self.bus_idx_red = np.array(np.zeros(self.n_units), dtype=[(key, int) for key in self.bus_ref_spec().keys()])
+
+    def bus_ref_spec(self):
+        return {'terminal': self.par['bus']}
+
+    def add_blocks(self):
+        p = self.par
+        self.pll = PLL1_nora(T_filter=self.par['T_pll'], bus=p['bus'])
+
+        self.pi_p = PIRegulator(K_p=p['P_K_p'], K_i=p['P_K_i'])
+        self.pi_p.input = lambda x, v: self.P_setp(x, v) - self.P(x, v) - p['K_SI']*self.pll.freq_est(x,v)
+
+        self.pi_q = PIRegulator(K_p=p['Q_K_p'], K_i=p['Q_K_i']) 
+        self.pi_q.input = lambda x, v: self.Q_setp(x, v) - self.Q(x, v)
+
+        self.lag_p = TimeConstant(T=p['T_i'])
+        self.lag_p.input = self.pi_p.output
+        self.lag_q = TimeConstant(T=p['T_i'])
+        self.lag_q.input = self.pi_q.output
+
+        self.I_d = self.lag_p.output
+        self.I_q = self.lag_q.output
+
+    def I_inj(self, x, v):
+        return (self.I_d(x, v) - 1j*self.I_q(x, v))*np.exp(1j*self.pll.output(x, v))
+
+    def input_list(self):
+        return ['P_setp', 'Q_setp']
+
+    def P(self, x, v):
+        v_n = self.sys_par['bus_v_n'][self.bus_idx_red['terminal']]
+        V = abs(v[self.bus_idx_red['terminal']])*v_n
+        return np.sqrt(3)*V*self.I_d(x, v)
+
+    def Q(self, x, v):
+        v_n = self.sys_par['bus_v_n'][self.bus_idx_red['terminal']]
+        V = abs(v[self.bus_idx_red['terminal']])*v_n
+        return np.sqrt(3)*V*self.I_q(x, v)
+
+    def load_flow_pq(self):
+        return self.bus_idx['terminal'], -self.par['P_setp'], -self.par['Q_setp']
+
+    def init_from_load_flow(self, x_0, v_0, S):
+        self._input_values['P_setp'] = self.par['P_setp']
+        self._input_values['Q_setp'] = self.par['Q_setp']
+
+        v_n = self.sys_par['bus_v_n'][self.bus_idx_red['terminal']]
+
+        V_0 = v_0[self.bus_idx_red['terminal']]*v_n
+
+        I_d_0 = self.par['P_setp']/(abs(V_0)*np.sqrt(3))
+        I_q_0 = self.par['Q_setp']/(abs(V_0)*np.sqrt(3))
+
+        self.pi_p.initialize(
+            x_0, v_0, self.lag_p.initialize(x_0, v_0, I_d_0)
+        )
+
+        self.pi_q.initialize(
+            x_0, v_0, self.lag_q.initialize(x_0, v_0, I_q_0)
+        )
+
+    def current_injections(self, x, v):
+        i_n = self.sys_par['s_n'] / (np.sqrt(3) * self.sys_par['bus_v_n'])
+        # self.P(x, v)
+        return self.bus_idx_red['terminal'], self.I_inj(x, v)/i_n[self.bus_idx_red['terminal']]
