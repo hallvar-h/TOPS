f8da0b73391d2fe0a02d8c888b37ecfdc6e739e7
jorgekso <146705858+jorgekso@users.noreply.github.com>
2024-11-07 09:04:18 +0100

    nothing new

--- inertia/Plot_SI.py
+++ inertia/Plot_SI.py
@@ -4,21 +4,13 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 results = []
 names = []
-folder_path = Path('Results/SC/sensitivity')
+folder_path = Path('Results/SI')
 for name in sorted(folder_path.iterdir()):
     with open(name,'r') as file:
         res = json.load(file)
         results.append(res)
-        formatted_string = name.stem.replace('_', '=')
+        names.append(name)
 
-        names.append(formatted_string)
-name = 'Results/SC/loss_HVDC.json'
-with open(name,'r') as file:
-    res = json.load(file)
-    results.append(res)
-    filename = name.split('/')[-1]
-    formatted_string = filename.replace('.json', '')
-    names.append(formatted_string)
 for res in results:
     for key, value in res.items():
         if key != 't':
@@ -41,20 +33,27 @@ for res in results:
 # i = 0
 # for res in results:
 #     bus7 = [row[10] for row in res['v']]
-#     plt.plot(res['t'], np.abs(np.array(bus7)),label = names[i] + ' bus 9')
+#     plt.plot(res['t'], np.abs(np.array(bus7)),label = str(names[i]) + ' bus 9')
 #     i+=1
 # plt.xlabel('Time [s]')
 # plt.ylabel('Bus voltage')
 # plt.legend()
 # # plt.show()
 # plt.figure()
+# i=0
 # for res in results:
-#     plt.plot(res['t'], np.abs(np.array(res['gen_P'])),label = res['gen_name'][0])
-# plt.legend()
-# fig = plt.figure()
-# plt.plot(res['t'], np.abs(res['VSC']))
+#     plt.plot(res['t'], np.abs(np.array(res['load_P'])),label = str(names[i]) + ' L1')
+#     i+=1
 # plt.xlabel('Time [s]')
 # plt.ylabel('MW')
+# plt.legend()
+
+
+
+fig = plt.figure()
+plt.plot(res['t'], np.abs(res['VSC_SI']))
+plt.xlabel('Time [s]')
+plt.ylabel('MW')
 
 
 plt.figure()
@@ -66,16 +65,16 @@ plt.xlabel('Time [s]')
 plt.ylabel('Frequency [Hz]')
 plt.grid()
 plt.legend()
-
-
-plt.figure()
-i = 0
-for res in results:
-    ROCOF = np.gradient(50+50*np.mean((res['gen_speed']),axis=1),res['t'])
-    plt.plot(res['t'],ROCOF, label = names[i])
-    i+=1
-plt.xlabel('Time [s]')
-plt.ylabel('ROCOF [Hz/s]')
-plt.grid()
-plt.legend()
 plt.show()
+
+# plt.figure()
+# i = 0
+# for res in results:
+#     ROCOF = np.gradient(50+50*np.mean((res['gen_speed']),axis=1),res['t'])
+#     plt.plot(res['t'],ROCOF, label = names[i])
+#     i+=1
+# plt.xlabel('Time [s]')
+# plt.ylabel('ROCOF [Hz/s]')
+# plt.grid()
+# plt.legend()
+# plt.show()
--- inertia/Write_to_JSON.py
+++ inertia/Write_to_JSON.py
@@ -12,7 +12,7 @@ import json
 if __name__ == '__main__':
     #for iteration in np.arange(550,900,50):
         # Load model
-        import Wind_and_SC as model_data
+        import Wind as model_data
         importlib.reload(model_data)
         model = model_data.load()
         model['loads'] = {'DynamicLoad': model['loads']}
@@ -33,15 +33,17 @@ if __name__ == '__main__':
 
 # ]}
     
-    model['vsc'] =  {'VSC_PQ_SI': [
-                ['name', 'bus', 'S_n', 'p_ref', 'q_ref',  'k_p', 'k_q', 'T_p', 'T_q', 'k_pll','T_pll', 'T_i', 'i_max', 'K_SI', 'T_rocof'],
-                ['VSC_SI', 'B1',    900,     0.7,       0.1,       1,      1,    0.1,   0.1,     5,      1,      0.01,    1.2, 100, 1],
-]}
-
-#     model['vsc'] =  {'VSC_PQ': [
-#                 ['name', 'bus', 'S_n', 'p_ref', 'q_ref',  'k_p', 'k_q', 'T_p', 'T_q', 'k_pll','T_pll', 'T_i', 'i_max'],
-#                 ['VSC1', 'B1',    900,     0.7,       0.1,       1,      1,    0.1,   0.1,     5,      1,      0.01,    1.2],
+#         model['vsc'] =  {'VSC_PQ_SI': [
+#                 ['name', 'bus', 'S_n', 'p_ref', 'q_ref',  'k_p', 'k_q', 'T_p', 'T_q', 'k_pll','T_pll', 'T_i', 'i_max', 'K_SI', 'T_rocof'],
+#                 ['VSC_SI', 'B1',    900,     0.7,       0.1,       1,      1,    0.1,   0.1,     5,      1,      0.01,    1.2, 0, 0.5],
+#                 ['VSC_HVDC', 'B8',    300,     1,       0.333,       1,      1,    0.1,   0.1,     5,      1,      0.01,    1.2, 0, 1],
 # ]}
+
+        model['vsc'] =  {'VSC_PQ': [
+                ['name', 'bus', 'S_n', 'p_ref', 'q_ref',      'k_p', 'k_q', 'T_p', 'T_q', 'k_pll','T_pll', 'T_i', 'i_max'],
+                ['VSC1', 'B1',    700,     1,       0.15,       0.1,      0.1,    1,   1,     5,      1,      1,    1.2],
+                ['VSC1', 'B8',    300,     1,       0.33,       0.1,      0.1,    1,   1,     5,      1,      1,    1.2],
+]}
     
 
 
@@ -82,8 +84,8 @@ if __name__ == '__main__':
         while t < t_end:
             sys.stdout.write("\r%d%%" % (t/(t_end)*100))
 
-            if 10 <= t:
-                ps.loads['DynamicLoad'].set_input('g_setp', 1.6, 0)
+            #if 10 <= t:
+                #ps.loads['DynamicLoad'].set_input('g_setp', 1.6, 0)
                 # ps.lines['Line'].event(ps, ps.lines['Line'].par['name'][0], 'disconnect')
 
             
@@ -125,31 +127,32 @@ if __name__ == '__main__':
 
 
 
-        # Store result
-        res['t'].append(t)
-        res['gen_speed'].append(ps.gen['GEN'].speed(x, v).copy())
-        res['v'].append(v.copy())
-        res['gen_I'].append(ps.gen['GEN'].I(x, v).copy())
-        res['gen_P'].append(ps.gen['GEN'].P_e(x, v).copy())
-        res['load_P'].append(ps.loads['Load'].P(x, v).copy())
-        res['load_Q'].append(ps.loads['Load'].Q(x, v).copy())
-        res['VSC_SI'].append(ps.vsc['VSC_PQ_SI'].p_e(x,v).copy())
-        res['bus_names'].append(ps.buses['name'])
+            # Store result
+            res['t'].append(t)
+            res['gen_speed'].append(ps.gen['GEN'].speed(x, v).copy())
+            res['v'].append(v.copy())
+            res['gen_I'].append(ps.gen['GEN'].I(x, v).copy())
+            res['gen_P'].append(ps.gen['GEN'].P_e(x, v).copy())
+            res['load_P'].append(ps.loads['DynamicLoad'].P(x, v).copy())
+            res['load_Q'].append(ps.loads['DynamicLoad'].Q(x, v).copy())
+            # res['VSC_SI'].append(ps.vsc['VSC_PQ_SI'].p_e(x,v).copy()*ps.vsc['VSC_PQ_SI'].par['S_n'])
+            res['VSC_SI'].append(ps.vsc['VSC_PQ'].p_e(x,v).copy())
+            res['bus_names'].append(ps.buses['name'])
 
         print('Simulation completed in {:.2f} seconds.'.format(time.time() - t_0))
         
 
-    for key, value in res.items():
-    # Iterate through the list of timesteps (assumed to be lists or arrays)
-        for i, timestep in enumerate(value):  # Use enumerate to modify the list in-place
-            if isinstance(timestep, np.ndarray):  # Check if it's a NumPy array
-                res[key][i] = timestep.tolist()  # Convert the NumPy array to a list
-    for key, value in res.items():
-        if(key != 't'):
+        for key, value in res.items():
         # Iterate through the list of timesteps (assumed to be lists or arrays)
             for i, timestep in enumerate(value):  # Use enumerate to modify the list in-place
-                for j, v in enumerate(res[key][i]):  # Iterate through each value in the timestep
-                    if isinstance(v, complex):  # Check if it's a complex number
-                        res[key][i][j] = str(v)  # Convert the complex number to a string
-    with open('Results/SI/test.json','w') as file:
-        json.dump(res,file)
+                if isinstance(timestep, np.ndarray):  # Check if it's a NumPy array
+                    res[key][i] = timestep.tolist()  # Convert the NumPy array to a list
+        for key, value in res.items():
+            if(key != 't'):
+            # Iterate through the list of timesteps (assumed to be lists or arrays)
+                for i, timestep in enumerate(value):  # Use enumerate to modify the list in-place
+                    for j, v in enumerate(res[key][i]):  # Iterate through each value in the timestep
+                        if isinstance(v, complex):  # Check if it's a complex number
+                            res[key][i][j] = str(v)  # Convert the complex number to a string
+        with open('Results/SI/base2.json','w') as file:
+            json.dump(res,file)
